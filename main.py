7546b5be711de00ea3faa6ec863c518576c7f217
Allen U <91634751+AsianBananaMan134@users.noreply.github.com>
2021-11-19 12:31:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rand
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -14,10 +15,19 @@ wn.bgpic("background.gif")
 apple = trtl.Turtle()
 letter = trtl.Turtle()
 
-#----- turtle stuff
+letter_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+random_letter = ""
+
+#-----turtle stuff-----
 letter.hideturtle()
 letter.penup()
+apple.penup()
 letter.color("white")
+
+#----- some stuff to know-----
+new_x = rand.randint(-180,180)
+new_y = rand.randint(0,200)
+falling = False
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -27,20 +37,33 @@ def draw_apple(active_apple):
 
 #-----functions-----
 def falling_apples():
-  letter.clear()
-  apple.penup()
-  wn.tracer(True)
-  apple.goto(apple.xcor(), -200)
-  wn.tracer(False)
+  global falling
+  if falling == False:
+    falling = True
+    letter.clear()
+    apple.penup()
+    wn.tracer(True)
+    apple.goto(apple.xcor(), -200)
+    wn.tracer(False)
+    apple.hideturtle()
+    apple.goto(new_x,new_y)
+    apple.showturtle()
+    draw_apple(apple)
+    write_letter()
+    wn.onkeypress(falling_apples, random_letter)
+    falling = False
 
 def write_letter():
+  global random_letter
+  random_letter = letter_list.pop(rand.randint(0, len(letter_list)-1))
+  random_letter = rand.choice(letter_list)
   letter.goto(apple.xcor()-18, apple.ycor()-40)
-  letter.write("A", font=("Arial", 55, "bold"))
+  letter.write(random_letter, font=("Arial", 55, "bold"))
 
 #-----function calls-----
 draw_apple(apple)
+apple.goto(new_x,new_y)
 write_letter()
-wn.onkeypress(falling_apples, "a")
 wn.listen()
-
+wn.onkeypress(falling_apples, random_letter)
 wn.mainloop()

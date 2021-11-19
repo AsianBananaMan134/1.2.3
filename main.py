55b37b41a02f236c9d7d818d8a2e2ba8992d6ccf
Allen U <91634751+AsianBananaMan134@users.noreply.github.com>
2021-11-19 13:19:09 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,58 +12,68 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 wn.addshape(pear_image) 
 wn.bgpic("background.gif")
 
-apple = trtl.Turtle()
-letter = trtl.Turtle()
+#-----the five apples-----
+apple_list = []
+apple_list_letters = []
 
-letter_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+letter_list = ["w","a","s","d"]
 random_letter = ""
 
-#-----turtle stuff-----
-letter.hideturtle()
-letter.penup()
-apple.penup()
-letter.color("white")
-
 #----- some stuff to know-----
-new_x = rand.randint(-180,180)
-new_y = rand.randint(0,200)
-falling = False
-
+for i in range(5): 
+    apple_list.append(trtl.Turtle())
+    apple_list_letters.append(rand.choice(letter_list))
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
-def draw_apple(active_apple):
-  active_apple.shape(apple_image)
-  wn.update()
+def draw_apple(i):
+  apple_list[(i)].penup() 
+  apple_list[i].shape(apple_image) 
+  wn.tracer(False) 
+  apple_list[i].setx(rand.randint(-175,175))
+  apple_list[i].sety(rand.randint(-25,100)) 
+  apple_list[i].sety(apple_list[i].ycor()-35) 
+  apple_list[i].color("white") 
+  apple_list[i].write(apple_list_letters[i],align="center",font=("Arial", 40, "bold")) 
+  apple_list[i].sety(apple_list[i].ycor()+35) 
+  apple_list[i].showturtle() 
+  wn.tracer(True) 
 
 #-----functions-----
-def falling_apples():
-  global falling
-  if falling == False:
-    falling = True
-    letter.clear()
-    apple.penup()
-    wn.tracer(True)
-    apple.goto(apple.xcor(), -200)
-    wn.tracer(False)
-    apple.hideturtle()
-    apple.goto(new_x,new_y)
-    apple.showturtle()
-    draw_apple(apple)
-    write_letter()
-    wn.onkeypress(falling_apples, random_letter)
-    falling = False
+def falling_apples(i):
+  apple_list[i].penup() 
+  apple_list[i].clear() 
+  apple_list[i].sety(-150) 
+  apple_list[i].hideturtle() 
+  draw_apple(i)
 
-def write_letter():
-  global random_letter
-  random_letter = letter_list.pop(rand.randint(0, len(letter_list)-1))
-  random_letter = rand.choice(letter_list)
-  letter.goto(apple.xcor()-18, apple.ycor()-40)
-  letter.write(random_letter, font=("Arial", 55, "bold"))
+def press_w():
+  for i in range(5):
+    if apple_list_letters[i]:
+      falling_apples(i)
+
+def press_a():
+  for i in range(5):
+    if apple_list_letters[i]:
+      falling_apples(i)
+
+def press_s():
+  for i in range(5):
+    if apple_list_letters[i]:
+      falling_apples(i)
+
+def press_d():
+  for i in range(5):
+    if apple_list_letters[i]:
+      falling_apples(i)
+
+for i in range(5):
+  draw_apple(i)
 
 #-----function calls-----
-draw_apple(apple)
-apple.goto(new_x,new_y)
-write_letter()
+wn.onkeypress(press_w, "w")
+wn.onkeypress(press_a, "a")
+wn.onkeypress(press_s, "s")
+wn.onkeypress(press_d, "d")
+
 wn.listen()
-wn.onkeypress(falling_apples, random_letter)
 wn.mainloop()
